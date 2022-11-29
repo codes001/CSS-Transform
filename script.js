13b4c31d3517852060dc4e823567220d939d0a32
Effiong Prince <95102901+Effiongprince19@users.noreply.github.com>
2022-11-29 14:37:45 +0100

    Updated JS

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ open_btn.addEventListener('click', () => {
 })
 
 close_btn.addEventListener('click', () => container.classList.toggle ('show'));
+
+
+// const open_btn = document.querySelector ('.open-btn');
+// const close_btn = document.querySelector ('.close-btn');
+// const container = document.querySelector('.container');
+// open_btn.addEventListener ('click', navToggle)
+
+// function navToggle () {
+//     btn.classList.toggle('open');
+//     container.classList.toggle('show');
+//     menu.classList.toggle('show-menu');
+// }
